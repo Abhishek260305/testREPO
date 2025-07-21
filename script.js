8d2c7c6cfe2327899e9bb867ba370326804cc705
Abhishek Thrivestack <abhishekthrivestack@Abhisheks-MacBook-Pro.local>
2025-07-21 12:35:55 +0530

    Add Google Analytics tracking and enhance script.js

--- script.js
+++ script.js
@@ -849,11 +849,9 @@ trackOnboardingStep('add_payment');
 trackOnboardingComplete();
 trackOnboardingSkip();
 // Onboarding user properties
-setThriveStackUserProperties({
-  favorite_genre: genre || 'unknown',
-  favorite_actor: actor || 'unknown',
-  preferred_country: country || 'unknown'
-});
+if (typeof genre !== 'undefined' && typeof actor !== 'undefined' && typeof country !== 'undefined') {
+  setThriveStackUserProperties({ favorite_genre: genre, favorite_actor: actor, preferred_country: country });
+}
 // ---------- ThriveStack Advanced Event Tracking ----------
 function trackThriveStackAccountAddedUser({ accountName, userEmail, userId, accountId }) {
   if (typeof thriveStack !== 'undefined' && typeof thriveStack.track === 'function') {
